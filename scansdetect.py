5e2b626b17b84d7dd0fac867eb18f9cf0522e50c
Nuri-Irfani <cranberrywalnut7@gmail.com>
2021-03-28 22:53:50 +0800

    feat: map IPs to set flags
    - each flags turned on in a packet is stored in a list, tcpFlags
    
    - IP and flags stored in dictionary

--- scansdetect.py
+++ scansdetect.py
@@ -1,138 +1,145 @@
-import argparse, os, sys, socket
+import argparse, os, sys, socket, datetime
 import dpkt, pyshark
+from collections import defaultdict
+from dpkt.compat import compat_ord
 from scapy.utils import RawPcapReader
 from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, TCP
+from tqdm import tqdm
 
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# scapy test: list all TCP packets. scapy is confusing.
-# need to find more tutorials
+
+# from dpkt's example code
 ###########################################################
-def pcap_Reader(file_name):
-    count = 0
-    TCP_count = 0
-    
-    for (pkt_data, pkt_metadata,) in RawPcapReader(file_name):
-        count += 1
-        
-        ether_pkt = Ether(pkt_data)
+def mac_addr(address):
+    return ':'.join('%02x' % compat_ord(b) for b in address)
 
-        # exclude non-IPv4 packets
-        if ether_pkt.type != 0x0800:
-            continue
 
-        # exclude non-TCP packet
-        ip_pkt = ether_pkt[IP]
-        if ip_pkt.proto != 6:
-            continue
-
-        TCP_count += 1
-
-    print('{} contains {} packets ({} TCP... maybe)'.
-          format(file_name, count, TCP_count))
-    
+# for sorting. Return negative if ip1 < ip2, 
+# 0 if they are equal, positive if ip1 > ip2.
+###########################################################
+def compare_IPs(ip1, ip2):
+    return sum(map(int, ip1.split('.'))) - sum(map(int, ip2.split('.')))
 
     
+
+# Set flags in TCP packet
+###########################################################
+def tcpFlags(tcp):
+    ret = list()
+
+    if tcp.flags & dpkt.tcp.TH_SYN  != 0:
+        ret.append('SYN')
+    if tcp.flags & dpkt.tcp.TH_RST  != 0:
+        ret.append('RST')
+    if tcp.flags & dpkt.tcp.TH_FIN != 0:
+        ret.append('FIN')
+    if tcp.flags & dpkt.tcp.TH_PUSH != 0:
+        ret.append('PSH')
+    if tcp.flags & dpkt.tcp.TH_ACK  != 0:
+        ret.append('ACK')
+    if tcp.flags & dpkt.tcp.TH_URG  != 0:
+        ret.append('URG')
+    if tcp.flags & dpkt.tcp.TH_ECE  != 0:
+        ret.append('ECE')
+    if tcp.flags & dpkt.tcp.TH_CWR  != 0:
+        ret.append('CWR')
+    
+    return ret
+
+
+
+
 # dpkt test: list all src and dst ips.
 ###########################################################
 def pcap_IPlist(file_name):
     
     #pass to dpkt's reader
     pcap = dpkt.pcap.Reader(file_name)
+    
+    flagchk = defaultdict(list) # Dictionary of suspects. suspect's IP: {# SYNs, # SYN-ACKs}
 
-    for (ts,buff) in pcap:
+    print('processing packets...')
+    
+    for timestamp, buf in pcap:    # iterate through packets
         try:
             #unpack ethernet frame to read data
-            eth = dpkt.ethernet.Ethernet(buff)
-            if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            continue 
+            try:
+                eth = dpkt.ethernet.Ethernet(buf)
+                if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+                    continue
+            except (dpkt.dpkt.UnpackError, IndexError):
+                continue
 
             #get IP packet
             ip = eth.data
-            if ip.p != dpkt.ip.IP_PROTO_TCP:
-            continue
+            if not ip:
+                continue
+
+            tcp = ip.data
+            if type(tcp) != dpkt.tcp.TCP:
+                continue
+
+            #get flags that are set in the packet
+            tcpFlag = tcpFlags(tcp)
+
 
             #convert IP addresses into string format.
             src = socket.inet_ntop(socket.AF_INET, ip.src)
             dst = socket.inet_ntop(socket.AF_INET, ip.dst)
 
-            # Print the source and destination IP
-            #sys.stdout = open('output.txt','wt')
-            with open('output.txt', 'a+') as f:
-                print('Source: ' +src+ ' Destination: '  +dst, file=f)
+            # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+            # obtained from example code provided by dpkt
+            do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+            more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+            fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+            
+            # put each IP and its list of set flags in a dictionary
+            # dictionary structure: 
+            # KEY: IP   VALUE: SYN, RST, FIN, PSH, ACK, URG, ECE, CWR
+            for flags in tcpFlag:
+                if flags == 'SYN':
+                    if src not in flagchk:
+                        flagchk[src] = {'SYN': 0, 'SYN-ACK': 0}
+                    flagchk[src]['SYN'] += 1
+                if flags == ('SYN','ACK'):
+                    if dst not in flagchk:
+                        flagchk[dst] = {'SYN': 0, 'SYN-ACK':0}
+                    flagchk[src]['SYN-ACK'] += 1
 
         except:
             pass
-        
-
-
-# Closed scans detection attempt (RST-ACK response)
-###########################################################
-def pcap_detClosed(file_name):
+            
     
-    #pass to dpkt's reader
-    pcap = dpkt.pcap.Reader(file_name)
-#    flagList = []
-    count = 0
-    
-    for (ts, buff) in pcap:
-        eth = dpkt.ethernet.Ethernet(buff)
-        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            continue     
-        
-        ip = eth.data
-        if ip.p != dpkt.ip.IP_PROTO_TCP:
-            continue
-        
-        tcp = ip.data           
-        if (tcp.flags & dpkt.tcp.TH_RST != 0) and (tcp.flags & dpkt.tcp.TH_ACK != 0):
-#            flagList.append(tcp)
-#        if ((tcp.flags & dpkt.tcp.TH_SYN != 0) and (tcp.flags & dpkt.tcp.TH_ACK != 0)):
-#            flagList.append(tcp)
-#        if ((tcp.flags & dpkt.tcp.TH_SYN != 0) and (tcp.flags & dpkt.tcp.TH_RST != 0) and (tcp.flags & dpkt.tcp.TH_ACK != 0)):
-#            flagList.append(tcp)
-#        if (tcp.flags & dpkt.tcp.TH_RST != 0):
-#            flagList.append(tcp)
-            count += 1
-        
-    print('There are ' +str(count)+ ' suspicious packets.')
-#    with open('suspects.txt', 'a+') as f:
-#        for i in range (len (flagList)):
-#            src = socket.inet_ntop(socket.AF_INET, ip.src)
-#            dst = socket.inet_ntop(socket.AF_INET, ip.dst)
-#            print('Source: ' +src+ ' Destination: '  +dst, flagList[i], file=f)
+    print('writing to txt file...')
+    sorted_flagchk = flagchk.items(),key=lambda
+    for i in tqdm(range(100)):
+        with open('output.txt', 'a+') as f:
+            for key, value in flagchk.items():
+                print("{a} has {b} \n".format(a=key, b=value), file=f)
+                #print('\nTimestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)), file=f)
+                #print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type, file=f)
+                #print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)' % (src, dst, ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset), file=f)
+                #print (tcpFlag, file=f)
 
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Argparse ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PCAP reader')
-    parser.add_argument('--pcap', metavar='<pcap file name>', help='count num of TCP packets')
     parser.add_argument('--listIP', type=argparse.FileType('rb'), metavar='<pcap file name>', help='list all src and dst IPs')
-    parser.add_argument('--scanClosed', type=argparse.FileType('rb'), metavar='<pcap file name>', help='attempts to detect RST-ACK responses')
     args = parser.parse_args()
 
-    if args.pcap:
-        file_name = args.pcap
-        if not os.path.isfile(file_name):
-            print('"{}" does not exist'.format(file_name), file=sys.stderr)
-            sys.exit(-1)
-        pcap_Reader(file_name)
 
     if args.listIP:
         file_name = args.listIP
         pcap_IPlist(file_name)
-        
-    if args.scanClosed:
-        file_name = args.scanClosed
-        pcap_detClosed(file_name)
+
 
 
     sys.exit(0)
